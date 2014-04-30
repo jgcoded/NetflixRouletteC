c0adbe98522733159f1ad6d1ca1c0816e80b2301
jgcoded <juliog@knights.ucf.edu>
2014-04-30 15:06:31 -0400

    nflx header: add web api wrappers

--- netflixroulettec.h
+++ netflixroulettec.h
@@ -17,6 +17,9 @@
 
 #define API extern
 
+#define NFLX_OK 1
+#define NFLX_BAD -1
+
 typedef enum
 {
 	NFLX_MOVIE=0,
@@ -30,9 +33,29 @@ API void nflx_init();
 
 API struct nflx* nflx_get_data(const char *title, const int year);
 
-API int nflx_release_year(struct nflx *media, int *year);
+API int nflx_get_unit(struct nflx *media, int* dest);
 
-API void nflx_destroy(struct nflx *n);
+API int nflx_get_showid(struct nflx *media, int* dest);
+
+API int nflx_get_release_year(struct nflx *media, int *dest);
+
+API int nflx_get_show_title(struct nflx *media, char *dest);
+
+API int nflx_get_rating(struct nflx *media, double *dest);
+
+API int nflx_get_category(struct nflx *media, char *dest);
+
+API int nflx_get_showcast(struct nflx *media, char* dest);
+
+API int nflx_get_director(struct nflx *media, char *dest);
+
+API int nflx_get_summary(struct nflx *media, char *dest);
+
+API int nflx_get_poster(struct nflx *media, char *dest);
+
+API int nflx_get_mediatype(struct nflx *media);
+
+API void nflx_destroy(struct nflx *media);
 
 API void nflx_deinit();
 
