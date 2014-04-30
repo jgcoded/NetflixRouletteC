cba7b905c0bbdcd6799a0133eb1ed1e71a5ade27
jgcoded <juliog@knights.ucf.edu>
2014-04-30 17:01:17 -0400

    Update test

--- test.c
+++ test.c
@@ -4,13 +4,29 @@
 
 int main(int argc, char* argv[])
 {
-	struct nflx *movie1;
+	struct nflx *movie;
 	
 	nflx_init();
 	
-	movie1 = nflx_get_data("The Boondocks", 2005);
+	movie = nflx_get_data("The Boondocks", 2005);
+
+	if (movie == NULL)
+		exit(1);
 	
-	nflx_destroy(movie1);
+	printf("unit: %d\n",         nflx_get_unit(movie));
+	printf("show id: %d\n",      nflx_get_showid(movie));
+	printf("release year: %d\n", nflx_get_release_year(movie));
+	printf("show title: %s\n",   nflx_get_show_title(movie));
+	printf("rating: %.1f\n",     nflx_get_rating(movie));
+	printf("category: %s\n",     nflx_get_category(movie));
+	printf("show cast: %s\n",    nflx_get_showcast(movie));
+	printf("directory: %s\n",    nflx_get_director(movie));
+	printf("summary: %s\n",      nflx_get_summary(movie));
+	printf("poster: %s\n",       nflx_get_poster(movie));
+	printf("media type: %s\n",   nflx_get_mediatype(movie) == NFLX_MOVIE
+													? "movie" : "show");
+
+	nflx_destroy(movie);
 
 	nflx_deinit();
 
